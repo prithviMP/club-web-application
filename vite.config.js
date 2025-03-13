dedbb422493b79aa6eb54797563d1a2fc2a39a01
prithviraj m pillai <prithvi_mp@yahoo.in>
2025-03-13 18:48:46 +0530

    Add Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000
+    port: 3000,
+    open: true,
   },
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',
     sourcemap: true,
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true
-      }
-    }
   },
-  resolve: {
-    alias: {
-      '@': '/src',
-      '@components': '/src/components',
-      '@pages': '/src/pages',
-      '@utils': '/src/utils'
-    }
-  }
-})
+});
